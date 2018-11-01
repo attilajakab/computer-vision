b0427458ae6ca33de96d9d3ef41409784650810b
attilajakab <attila.jakab@globallogic.com>
2018-11-01 17:31:13 +0100

    Remove commented out lines.

--- client/app.js
+++ client/app.js
@@ -20,23 +20,4 @@ socket.on('frame', function (data) {
   canvas.height = data.rows;
   canvas.width = data.cols;
   context.putImageData(imgData, 0, 0);
-
-
-
-  // // Reference: http://stackoverflow.com/questions/24107378/socket-io-began-to-support-binary-stream-from-1-0-is-there-a-complete-example-e/24124966#24124966
-  // var uint8Arr = new Uint8Array(data.buffer);
-  // var b64encoded = bufferToBase64(uint8Arr);
-  // console.log(b64encoded);
-  // myImg.src = 'data:image/png;base64,' + b64encoded;
-
-
-
-
-  // var str = String.fromCharCode.apply(null, uint8Arr);
-  // var base64String = btoa(str);
-
-  // img.onload = function () {
-  //   context.drawImage(this, 0, 0, canvas.width, canvas.height);
-  // };
-  // img.src = 'data:image/png;base64,' + base64String;
 });
